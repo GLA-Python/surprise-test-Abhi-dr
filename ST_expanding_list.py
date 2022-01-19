c6add103cb2f16804d6b7a02ef2f09f915a6dc33
Divyanshu Khandelwal <67157526+Abhi-dr@users.noreply.github.com>
2022-01-19 11:44:04 +0530

    Update ST_expanding_list.py

--- ST_expanding_list.py
+++ ST_expanding_list.py
@@ -1,17 +1,21 @@
+def check_data(data):
+    result = []
+
+    for a in range(len(data) - 1):
+        result.append(abs(data[a + 1] - data[a]))
+
+    check = []
+
+    for a, b in zip(result, result[1:]):
+        if a < b:
+            check.append(True)
+        else:
+            check.append(False)
+
+    return all(check)
+
+
 data = eval(input("Enter the list: "))
 
-result = []
-
-for a in range(len(data)-1):
-    result.append(abs(data[a + 1] - data[a]))
-
-check = []
-
-for a, b in zip(result, result[1:]):
-    if a < b:
-        check.append(True)
-    else:
-        check.append(False)
-
-print(all(check))
+print(check_data(data))
 
